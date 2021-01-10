7eeb246e2bc3cc62dfb6da36b3b8476e777caae6
IsinghGitHub <Indrajitsingh37@gmail.com>
2021-01-10 19:09:55 +0530

    Create char-2.java

---
+++ Java Programming/char-2.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    	
+    char letter1 = '9';
+    System.out.println(letter1);  // prints 9
+    	
+    char letter2 = 75;
+    System.out.println(letter2);  // prints A
+
+  }
+}
