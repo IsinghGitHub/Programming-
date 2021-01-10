3a4d13bdf70e726ef68b468157c016caf67dc39c
IsinghGitHub <Indrajitsingh37@gmail.com>
2021-01-10 19:00:57 +0530

    Create int.java

---
+++ Java Programming/int.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    	
+    int range = -4250000;
+    System.out.println(range);  // print -4250000
+  }
+}
+
+// The int data type can have values from -231 to 231-1 (32-bit signed two's complement integer).
