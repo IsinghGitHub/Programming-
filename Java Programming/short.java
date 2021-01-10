4cb36046da05794ea8005d3ecedfb2c605e538a9
IsinghGitHub <Indrajitsingh37@gmail.com>
2021-01-10 18:59:27 +0530

    Create short.java

---
+++ Java Programming/short.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    	
+    short temperature;  // The short data type in Java can have values from 32768 to 32767 (16-bit signed two's complement integer).
+    temperature = -200;
+    System.out.println(temperature);  // prints -200
+  }
+}
